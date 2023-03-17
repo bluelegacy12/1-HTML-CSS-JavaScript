378c3c63298b526cc1dddee111cb74c932b8e127
Elza <dylan.elza@gmail.com>
2023-03-17 11:57:47 -0400

    updating files

---
+++ Week 3 Java Intro/js/javascript-hello-world.js
@@ -0,0 +1,14 @@
+function getName() {
+    const userName = prompt("What is your name?");
+    sayHello(userName);
+}
+function sayHello(getName) {
+    alert("Hello " + getName + "!");
+    console.log('Alert sent!');
+}
+
+if age > 12 && age < 65 {
+    return $20;
+} else {
+    return $10;
+}
